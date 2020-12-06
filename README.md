514213e2006b9f6ecaab3779d18c583a4f84e34d
cocarica53120 <alain.collet2@gmail.com>
2020-12-06 19:03:48 +0100

    Use of Spring/Web Second version of Controller Added Product class => Intro to bean Jackson make json <=> class sur le bean Product Le Spring Web a permis de faire une API sans parser de http et faire de conversion classe <=> json

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-Weblink:
-
 # TutoJavaSpring"
 
-https://openclassrooms.com/fr/courses/4668056-construisez-des-microservices/5122884-creez-un-microservice-grace-a-spring-boot
-"
+[Weblink](https://openclassrooms.com/fr/courses/4668056-construisez-des-microservices/5122884-creez-un-microservice-grace-a-spring-boot)
+
+# Use of Spring Initializr 
+<img src="SpringInitializr.png"
+    alt="Markdown Monster icon"
+    style="float: left; margin-right: 10px;" />
+
+# Add Product class with getter/setter + constructor.
+On GetMapping(id), return the instance of Product...
+Add JSON viewer extension in chrome....
--- demo/src/main/java/com/example/demo/web/controller/ProductController.java
+++ demo/src/main/java/com/example/demo/web/controller/ProductController.java
@@ -1,26 +1,20 @@
-package com.example.demo;
+package com.example.demo.web.controller;
 
+import com.example.demo.model.Product;
 import org.springframework.web.bind.annotation.*;
-
-class Product {
-
-    Product() {
-        System.out.println("ctor " + Product.class);
-    }
-
-}
 
 @RestController
 public class ProductController {
     @RequestMapping(value="/products", method = RequestMethod.GET)
     public String listeProduits() {
-        return new Product().toString();
+        return "Vous avez demandé la liste des produits";
     }
 
     @GetMapping(value="/products/{id}")
-    public String afficherProduit(@PathVariable int id) {
+    public Product afficherProduit(@PathVariable int id) {
         System.out.println("In afficherProduit with params=" + id);
-        return "Vous avez demandé un produit avec l'id = " + id;
+        Product product = new Product(id, new String("Apsirateur"), 100);
+        return product;
     }
 
 }
