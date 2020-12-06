204e28fb6eb5534bf1299d520c444715a85be49d
cocarica53120 <alain.collet2@gmail.com>
2020-12-06 22:09:45 +0100

    Use of Spring/Web Fourth version Added simple POST

--- README.md
+++ README.md
@@ -10,3 +10,10 @@
 # Add Product class with getter/setter + constructor.
 On GetMapping(id), return the instance of Product...
 Add JSON viewer extension in chrome....
+
+
+Curl commands:
+curl -X GET localhost:9090/products
+curl -X GET localhost:9090/products/1
+curl -L -X POST "localhost:9090/products" -H 'Content-Type: application/json' --data-raw '{"id": 4, "name": "Lave Linge", "price": 752}'
+
--- demo/src/main/java/com/example/demo/web/controller/ProductController.java
+++ demo/src/main/java/com/example/demo/web/controller/ProductController.java
@@ -10,15 +10,15 @@ import java.util.List;
 @RestController
 public class ProductController {
 
-//    @Autowired // Permet à Spring de créer ube instance de ProductDaoImpl
+    @Autowired // Permet à Spring de créer ube instance de ProductDaoImpl
     private ProductDaoImpl productDao;
 
     // If @Autowired (with private ProductDaoImpl productDao;) as above is not used, instanciates the productDao
     // It works, but not sure that it is the good practice.
-    ProductController() {
-        System.out.println("ctor ProductDaoImpl");
-        productDao = new ProductDaoImpl();
-    }
+//    ProductController() {
+//        System.out.println("ctor ProductDaoImpl");
+//        productDao = new ProductDaoImpl();
+//    }
 
     @RequestMapping(value="/products", method = RequestMethod.GET)
     public List<Product> listProducts() {
@@ -28,6 +28,11 @@ public class ProductController {
     @GetMapping(value="/products/{id}")
     public Product getProduct(@PathVariable int id) {
         return productDao.findById(id);
+    }
+
+    @PostMapping(value="/products")
+    public void AddProduct(@RequestBody Product product) {
+        productDao.save(product);
     }
 
 
